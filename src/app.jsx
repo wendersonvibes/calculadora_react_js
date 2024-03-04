cbb597c37c11019faeb743bf32f7bd4a6a574833
wenderson <110772284+wendersonvibes@users.noreply.github.com>
2024-03-04 10:52:08 -0300

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,44 @@
 import { useState } from 'react';
 import './app.css'
+import { parse } from 'postcss';
 
 export function App() {
-
+  
   const [num, setNum] = useState(0)
   const [operacao, setOperacao] = useState("")
+  const [oldNum, setOldNum] = useState(0)
 
   const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const operacoes = ["*", "/", "+", "-", "=",];
 
   const handleNum = (e) => {
     let numClicado = e.target.value;
-    setNum(num + numClicado);
+    num === 0 // CONDIÇÃO
+    ? setNum(numClicado) // VALOR SE FOR TRUE
+    : setNum(num + numClicado); // VALOR SE FOR FALSE 
   }
 
-  const handleOperador = (e) => {
-    let operador = e.target.value;
-    setOperacao(operador);
+  const handleOperacao = (e) => {
+    let operacaoInput = e.target.value;
+    setOperacao(operacaoInput);
+
+    setOldNum(num);
+    setNum(0)
   };
+
+  const calcular = () => {
+    let resultado;
+    if(operacao == "/") {
+      resultado = parseFloat(oldNum) / parseFloat(num)
+    } 
+    else if(operacao == "*" ) {
+      resultado =(parseFloat(oldNum) * parseFloat(num))
+    }
+    else if(operacao == "-") {   
+      resultado = (oldNum - num)
+    }
+    setNum(resultado)
+  }
 
   const limpar = () => {
     setNum(0);
@@ -30,7 +51,7 @@ export function App() {
 
       <div className='calculadora max-w-md rounded-lg m-auto mt-12'>
         <p className='w-full h-32 rounded-t-lg bg-slate-400 p-6 text-4xl flex justify-end'>
-          {num} {operacao}
+          {num}
         </p>
 
         <div className="p-6">
@@ -52,7 +73,7 @@ export function App() {
               <button
                 className='bg-amber-500 hover:bg-amber-700 text-white font-bold p-5 rounded-full'
                 value={operador}
-                onClick={handleOperador}>
+                onClick={handleOperacao}>
                 {operador}
               </button>
             )
@@ -62,6 +83,12 @@ export function App() {
             className='bg-amber-500 hover:bg-amber-700 text-white font-bold p-5 rounded-full'
             onClick={limpar}>
             AC
+          </button>
+
+          <button
+            className='bg-amber-500 hover:bg-amber-700 text-white font-bold p-5 rounded-full'
+            onClick={calcular}>
+            =
           </button>
         </div>
       </div>
